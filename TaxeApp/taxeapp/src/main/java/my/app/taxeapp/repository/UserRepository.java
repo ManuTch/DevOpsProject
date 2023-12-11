473c329c1353a2db8f433dc60fc6e87b06809ef4
sara ah <134241901+sara680@users.noreply.github.com>
2023-12-11 16:31:33 +0100

    Delete TaxeApp directory

--- TaxeApp/taxeapp/src/main/java/my/app/taxeapp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package my.app.taxeapp.repository;
-
-import my.app.taxeapp.bean.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
