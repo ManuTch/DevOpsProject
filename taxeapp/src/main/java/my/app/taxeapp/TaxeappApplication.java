d283d3d42be01a1921f84afb4d0af3691943a1f6
houdafr <106031888+houdafr@users.noreply.github.com>
2023-12-15 14:29:23 +0100

    Add files via upload

---
+++ taxeapp/src/main/java/my/app/taxeapp/TaxeappApplication.java
@@ -0,0 +1,13 @@
+package my.app.taxeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxeappApplication.class, args);
+	}
+
+}
